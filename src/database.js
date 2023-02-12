55998416c09d37a49af96970ee4a315e53e58f03
AlessandraMayumi <alessandra_mms@hotmail.com>
2023-02-12 15:45:25 -0300

    database as file - fs module

--- src/database.js
+++ src/database.js
@@ -1,15 +1,23 @@
+import fs from 'node:fs/promises';
+
 export class Database {
-    database = {}
+    #database = {}
+    #persist() {
+        fs.writeFile('db.json', JSON.stringify(this.#database))
+    }
+
     select(table) {
-        const data = this.database[table] ?? []
+        const data = this.#database[table] ?? []
         return data
     }
+
     insert(table, data) {
-        if(Array.isArray(this.database[table])) {
-            this.database[table].push(data);
+        if (Array.isArray(this.#database[table])) {
+            this.#database[table].push(data);
         } else {
-            this.database[table] = [data];
+            this.#database[table] = [data];
         }
+        this.#persist();
         return data;
     }
 }
