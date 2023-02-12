2e1ff489b88dc9718a410bd1ad7e8da7d4349181
AlessandraMayumi <alessandra_mms@hotmail.com>
2023-02-12 16:17:42 -0300

    remove routes from server init

--- src/server.js
+++ src/server.js
@@ -1,33 +1,21 @@
 import http from 'node:http';
-import { randomUUID } from 'node:crypto';
 import { json } from './middlewares/json.js'
-import { Database } from './database.js';
-
-const database = new Database();
+import { routes } from './routes.js';
 
 const server = http.createServer(async (req, res) => {
     const { method, url } = req;
 
     await json(req, res);
 
-    if (method === 'GET' && url === '/users') {
-        const users = database.select('users')
-        return res
-            .setHeader('Content-type', 'application/json')
-            .end(JSON.stringify(users))
+    const route = routes.find(r => {
+        return r.method === method && r.path === url;
+    })
+
+    if(route) {
+        return route.handler(req, res);
     }
 
-
-    if (method === 'POST' && url === '/users') {
-        const { name, email } = req.body;
-        const user = {
-            id: randomUUID(),
-            name,
-            email,
-        }
-        database.insert('users', user)
-        return res.end('Criação de usuário')
-    }
+    return res.writeHead(404).end();
 })
 
 server.listen(3333)
